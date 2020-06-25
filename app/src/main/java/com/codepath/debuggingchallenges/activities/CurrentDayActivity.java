af1d3a9610f8ee9804e763cc14ef4aac08c657fa
LucasG234 <lucasgates234@gmail.com>
2020-06-25 11:01:20 -0500

    Fixed bug in CurrentDayActivity

--- app/src/main/java/com/codepath/debuggingchallenges/activities/CurrentDayActivity.java
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/CurrentDayActivity.java
@@ -2,6 +2,8 @@ package com.codepath.debuggingchallenges.activities;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.widget.TextView;
 
 import com.codepath.debuggingchallenges.R;
@@ -11,13 +13,18 @@ import java.util.Calendar;
 public class CurrentDayActivity extends AppCompatActivity {
 
     TextView tvDay;
+    public static final String TAG = "CurrentDayActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "1");
         setContentView(R.layout.activity_current_day);
+        Log.d(TAG, "2");
         tvDay = (TextView) findViewById(R.id.tvDay);
-        tvDay.setText(getDayOfMonth());
+        Log.d(TAG, "3");
+        Log.d(TAG, "Result of getDayOfMonth: " + getDayOfMonth());
+        tvDay.setText("" + getDayOfMonth());
     }
 
     private int getDayOfMonth() {
